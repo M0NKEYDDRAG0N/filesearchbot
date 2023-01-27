51c2d4fa250952582e8cf2c9392467d71f2068b6
Dracula Coders <112293593+draculacoders@users.noreply.github.com>
2023-01-27 13:40:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/draculacoders/filesearchbot /filesearchbot
+  git clone https://github.com/draculacoders/filesearchbot.git /filesearchbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /filesearchbot
-
+fi
 cd /filesearchbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
